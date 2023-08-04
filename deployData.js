12c4ae26e21cd118d75271f93dc6d59b8c9ba7e2
Fathan Nur Sidiq <fathan.nursidiq2039@students.unila.ac.id>
2023-08-04 11:46:33 +0700

    first commit

---
+++ deployData.js
@@ -0,0 +1,7 @@
+const studentName = 'Aldi Ersalado';
+
+const studentId = '220401010292';
+
+const NetlifyDeployUrl = 'https://646ccff22b3bca05f139c211--polite-kitten-a55dac.netlify.app/client/index.html'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
